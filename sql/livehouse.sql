11174b5087218204579bdbccfe9fcf06883d14ab
github lucashigashinogit config --global user.name github <lucashigashino@gmail.com>
2024-03-10 19:05:46 +0900

    add データ関係の修正

--- sql/livehouse.sql
+++ sql/livehouse.sql
@@ -1,21 +1,29 @@
-INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('UrBANGUILD', 35.0237056, 135.7545413, 'https://urbanguild.net/'); INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('LIVE HOUSE DEWEY', 35.0237009, 135.7579916, 'https://kiyamachi-dewey.com/'); INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('アークデュウ', 35.0172265, 135.7590776, 'http://arcdeux.net/'); INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('和音堂', 35.0237056, 135.7545413, 'https://hasekazune.wixsite.com/home'); INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('someno kyoto', 35.0237009, 135.7579916, 'https://www.someno.kyoto/'); INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('T's studio', 35.0172265, 135.7590776, 'https://tsstudio.jp/'); INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('The Velvet Note', 35.021234, 135.756789, 'https://thevelvetnote.live%27/); INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('Cosmic Groove', 35.019876, 135.758901, 'https://cosmicgroove.xyz%27/); INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('Soul Sanctuary', 35.022345, 135.755432, 'https://soulsanctuary.live%27/);
+INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('UrBANGUILD', 35.00791281456209, 135.7709916205908, 'https://urbanguild.net/');
+INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('LIVE HOUSE DEWEY', 35.007449991143126, 135.76972949968342, 'https://kiyamachi-dewey.com/'); 
+INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('アークデュウ', 35.000185309093155, 135.76834796713865, 'http://arcdeux.net/'); 
+INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('和音堂', 34.998889855749475, 135.73125135364745, 'https://hasekazune.wixsite.com/home'); 
+INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('someno kyoto', 35.00660461751369, 135.7697844229576, 'https://www.someno.kyoto/'); 
+INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('Ts studio', 35.0172265, 135.7590776, 'https://tsstudio.jp/'); 
+INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('The Velvet Note', 35.021234, 135.756789, 'https://thevelvetnote.live%27/'); 
+INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('Cosmic Groove', 35.019876, 135.758901, 'https://cosmicgroove.xyz%27/'); 
+INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES ('Soul Sanctuary', 35.022345, 135.755432, 'https://soulsanctuary.live%27/');
 INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES
-('四条河原町 SOUND CRASH!!', 35.001234, 135.765432, 'https://example.com/soundcrash');
+('京都nano', 35.01274090637064, 135.7553839543097, 'https://livehouse-nano.com/index.php');
 INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES
-('京都MOJO', 35.002345, 135.766543, 'http://kyoto-mojo.com/');
+('京都MOJO', 35.00411123708301, 135.75684734518285, 'http://kyoto-mojo.com/');
 INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES
-('京都Station Live Nexus', 35.003456, 135.767654, 'https://example.com/nexus');
+('京都GROWLY', 35.013577144107224, 135.74164420406552, 'https://growly.net/');
 INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES
-('磔磔', 35.004567, 135.768765, 'https://www.geisya.or.jp/~takutaku/');
+('磔磔', 35.00150894650826, 135.76432216812057, 'https://www.geisya.or.jp/~takutaku/');
 INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES
-('KYOTO MUSE', 35.005678, 135.769876, 'http://muse-live.com/kyoto/');
+('KYOTO MUSE', 35.00449694310253, 135.76376426863322, 'http://muse-live.com/kyoto/');
 INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES
 ('METRO', 35.006789, 135.770987, 'https://www.metro.ne.jp/');
 INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES
-('山科音座', 35.007890, 135.771108, 'https://example.com/onza');
+('拾得', 35.02068339396688, 135.74875474054227, 'https://www.jittoku.com/');
 INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES
-('伏見SOUNDHOUSE', 35.008901, 135.772219, 'https://example.com/soundhouse');
+('PUB VOXhall', 35.03017207508799, 135.75750947057335, 'http://voxhall.net/');
 INSERT INTO livehouse (livehouseName, longitude, latitude, homePage) VALUES
-('右京ライブハウス', 35.009102, 135.773320, 'https://example.com/kyotolivehouse');
+('右京ライブハウス', 35.02519761731725, 135.7589868875881, 'https://example.com/kyotolivehouse');
 
 17件
